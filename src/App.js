5c66a7cedde7c75337f23624f662f69564893db6
John Wells <molekewl2017@gmail.com>
2019-08-19 03:46:17 -0700

    connect spotify api | you can search artist name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_ADDRESS = 'http://spotify-api-wrapper.appspot.com';
+
+class App extends Component {
+  state = { artistQuery: '', artist: null, tracks: [] };
+
+  updateArtistQuery = event => {
+    this.setState({ artistQuery: event.target.value});
+  }
+
+  handleKeyPress = event => {
+    if (event.key === 'Enter') {
+      this.searchArtist();
+    }
+  }
+
+  searchArtist = () => {
+    console.log('this.state', this.state);
+
+    fetch(`${API_ADDRESS}/artist/${this.state.artistQuery}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log('json', json);
+
+        if (json.artists.total > 0) {
+          const artist = json.artists.items[0];
+
+          console.log('artist', artist);
+          this.setState({ artist });
+
+          fetch(`${API_ADDRESS}/artist/${artist.id}/top-tracks`)
+            .then(response => response.json())
+            .then(json => this.setState({ tracks: json.tracks }))
+            .catch(error => alert(error.message));
+        }
+      })
+      .catch(error => alert(error.message));
+  }
+
+  render() {
+    console.log('this.state', this.state);
+
+    return (
+      <div className="App">
+        <h2>ReactJS - Spotify Search</h2>
+        <input 
+          onChange={this.updateArtistQuery}
+          onKeyPress={this.handleKeyPress}
+          placeholder='Search for an Artist' />
+        <button onClick={this.searchArtist}>Search</button>
+      </div>
+    );
+  }
 }
 
 export default App;
